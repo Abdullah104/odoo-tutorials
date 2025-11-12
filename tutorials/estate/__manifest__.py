b781c6d763cf77f823895381e30a643330810c4b
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-11-12 21:31:46 +0300

    Chapter 6: Basic Views

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -6,6 +6,9 @@
     ],
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_property_list_view.xml",
+        "views/estate_property_form_view.xml",
+        "views/estate_property_search_view.xml",
         "views/estate_property_views.xml",
         "views/estate_menus.xml",
     ]
--- tutorials/estate/models/estate_property.py
+++ tutorials/estate/models/estate_property.py
@@ -7,18 +7,19 @@ class RealEstate(models.Model):
     _name = 'estate.property'
     _description = 'Real estate property'
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string="Title")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months=3))
+    date_availability = fields.Date(copy=False, default=fields.Date.today() + relativedelta(months=3),
+                                    string="Available From")
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
     active = fields.Boolean(default=True)
