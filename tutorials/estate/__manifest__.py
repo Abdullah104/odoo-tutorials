a24c144f6184888c6bfe8be2fedff54587bc24d6
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-12-13 12:13:02 +0300

    Server framework 101 - Chapter 14: A Brief History Of QWeb

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -8,6 +8,7 @@
         "security/ir.model.access.csv",
         "views/estate_property/estate_property_list_view.xml",
         "views/estate_property/estate_property_form_view.xml",
+        "views/estate_property/estate_property_kanban_view.xml",
         "views/estate_property/estate_property_search_view.xml",
         "views/estate_property/estate_property_views.xml",
         "views/estate_property_offer/estate_property_offer_views.xml",
