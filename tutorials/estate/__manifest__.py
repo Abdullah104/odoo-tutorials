7fcd752c53bf6f774809af4d094ec8d4298a5c45
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-12-14 14:54:42 +0300

    update tag form to include color picker and display tag color in kanban view

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -19,6 +19,7 @@
         "views/estate_property_type/estate_property_type_list_view.xml",
         "views/estate_property_tag/estate_property_tag_views.xml",
         "views/estate_property_tag/estate_property_tag_list_view.xml",
+        "views/estate_property_tag/estate_property_tag_form_view.xml",
         "views/estate_menus.xml",
         "views/res_users/res_users_properties_view.xml",
     ]
