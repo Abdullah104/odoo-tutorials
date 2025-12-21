21332976bd8019595f4a3f4be97ff425d50e6d59
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-12-21 15:07:24 +0300

    Tutorials - Discover the web framework - Chapter 1: Owl components - 4. Using markup to display html

--- tutorials/awesome_owl/static/src/playground.js
+++ tutorials/awesome_owl/static/src/playground.js
@@ -1,4 +1,4 @@
-import { Component } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -6,4 +6,8 @@ export class Playground extends Component {
   static template = "awesome_owl.playground";
 
   static components = { Counter, Card };
+
+  cardContent = "<div class='text-primary'>some content</div";
+  card1Content = this.cardContent;
+  card2Content = markup(this.cardContent);
 }
