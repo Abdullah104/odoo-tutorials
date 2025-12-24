136897010121894b9f31361e59f38b327face8a7
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-12-24 20:40:57 +0300

    Tutorials - Discover the web framework - Chapter 1: Owl components - 6. The sum of two Counter

--- tutorials/awesome_owl/static/src/playground.js
+++ tutorials/awesome_owl/static/src/playground.js
@@ -7,7 +7,15 @@ export class Playground extends Component {
 
   static components = { Counter, Card };
 
+  setup() {
+    this.state = useState({ sum: 2 });
+
+    this.incrementSum = this.incrementSum.bind(this);
+  }
+
   cardContent = "<div class='text-primary'>some content</div";
   card1Content = this.cardContent;
   card2Content = markup(this.cardContent);
+
+  incrementSum = () => this.state.sum++;
 }
