86f102549f3e246cfcda797de9ba6563e77c8a81
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2026-01-26 08:15:37 +0300

    2. Add some buttons for quick navigation

--- tutorials/awesome_dashboard/static/src/dashboard.js
+++ tutorials/awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,31 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
 
 class AwesomeDashboard extends Component {
   static template = "awesome_dashboard.AwesomeDashboard";
   static components = { Layout };
+
+  setup() {
+    this.action = useService("action");
+  }
+
+  openCustomers() {
+    this.action.doAction("base.action_partner_form");
+  }
+
+  openLeads(activity) {
+    this.action.doAction({
+      type: "ir.actions.act_window",
+      name: _t("Leads"),
+      target: "current",
+      res_id: activity.res_id,
+      res_model: "crm.lead",
+      views: [[false, "form"]],
+    });
+  }
 }
 
 registry
